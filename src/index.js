dc68bd887b098168481fc63fe9596bda98047483
gradseb <kros00133@gmail.com>
2024-05-07 23:22:09 +0200

    1234

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-// // import { fetchCountries } from './fetchCountries.js';
-
-// const countrySearcher = document.querySelector('#search-box');
-// const countryList = document.querySelector('.country-list');
-// const countryInfo = document.querySelector('.country-info');
-// const debounce = require('lodash.debounce');
-
-// const clearMarkup = ref => (ref.innerHTML = '');
-
-// const inputHandler = debounce(txt => {
-//   const textInput = txt.target.value.trim();
-
-//   if (textInput.lenght >= 2) {
-//     handleSearch(textInput);
-
-//     clearMarkup(countryList);
-//     clearMarkup(countryInfo);
-//     return;
-//   }
-//   // handleSearch(textInput); // Wywołanie funkcji handleSearch po wprowadzeniu tekstu
-// }, 300);
-
-// const { fetchCountries } = require('./fetchCountries');
-// async function handleSearch(countryName) {
-//   try {
-//     const countries = await fetchCountries(countryName);
-//     console.log('Countries found: ', countries);
-//   } catch (error) {
-//     console.error('Error fetching countries: ', error.message);
-//   }
-// }
-
 const inputText = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
@@ -61,7 +29,7 @@ async function handleSearch(countryName) {
       notiflix.Notify.info(
         'Too many matches found. Please enter a more specific name.'
       );
-    } else if (countries.length >= 2 && countries.length <= 10) {
+    } else if (countries.length <= 10) {
       displayCountries(countries);
     } else if (countries.length === 1) {
       displayCountryInfo(countries[0]);
@@ -76,7 +44,7 @@ function displayCountries(countries) {
   countries.forEach(country => {
     const li = document.createElement('li');
     li.textContent = country.name.official;
-    li.addEventListener('click', () => displayCountryInfo(country)); // Dodajemy nasłuchiwacz kliknięcia
+    li.addEventListener('click', () => displayCountryInfo(country));
     countryList.appendChild(li);
   });
 }
@@ -88,7 +56,7 @@ function displayCountryInfo(country) {
         <h2>${country.name.official}</h2>
         <p>Capital: ${country.capital}</p>
         <p>Population: ${country.population}</p>
-        <img src="${country.flags.png}" alt="Flag of ${country.name.official}">
+        <img src="${country.flags.svg}" alt="Flag of ${country.name.official}">
         <p>Languages: ${Object.values(languages)}</p>
     `;
   countryInfo.appendChild(div);
