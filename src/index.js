e414a09878d5e895bc7b770539eed6789a5bc935
gradseb <kros00133@gmail.com>
2024-05-09 16:56:37 +0200

    moduł 10

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const inputText = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-const debounce = require('lodash.debounce');
-const notiflix = require('notiflix');
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
+const debounce_delay = 400;
 
 const clearMarkup = ref => (ref.innerHTML = '');
 
@@ -18,24 +20,22 @@ const inputHandler = debounce(txt => {
   handleSearch(textInput);
 }, 300);
 
-inputText.addEventListener('input', inputHandler);
-
-const { fetchCountries } = require('./fetchCountries');
-async function handleSearch(countryName) {
+import { fetchCountries } from './fetchCountries';
+async function handleSearch(name) {
   try {
-    const countries = await fetchCountries(countryName);
+    const countries = await fetchCountries(name);
 
     if (countries.length > 10) {
-      notiflix.Notify.info(
-        'Too many matches found. Please enter a more specific name.'
-      );
+      clearMarkup(countryList);
+      clearMarkup(countryInfo);
+      Notify.info('Too many matches found. Please enter a more specific name.');
     } else if (countries.length <= 10) {
       displayCountries(countries);
     } else if (countries.length === 1) {
       displayCountryInfo(countries[0]);
     }
   } catch (error) {
-    notiflix.Notify.failure('Oops, there is no country with that name.');
+    Notify.failure('Oops, there is no country with that name.');
   }
 }
 
@@ -44,7 +44,7 @@ function displayCountries(countries) {
   countries.forEach(country => {
     const li = document.createElement('li');
     li.textContent = country.name.official;
-    li.addEventListener('click', () => displayCountryInfo(div.innerHTML));
+    li.addEventListener('click', () => displayCountryInfo(country));
     countryList.appendChild(li);
   });
 }
@@ -52,12 +52,25 @@ function displayCountries(countries) {
 function displayCountryInfo(country) {
   clearMarkup(countryInfo);
   const div = document.createElement('div');
+  const languages = country.languages;
+
+  let languagesString = '';
+
+  // Iterujemy po kluczach i wartościach obiektu country.languages
+  for (const lang in languages) {
+    // Dodajemy nazwę języka do ciągu znaków
+    languagesString += `${languages[lang]} `;
+  }
   div.innerHTML = `
-        <h2>${country.name.official}</h2>
-        <p>Capital: ${country.capital}</p>
-        <p>Population: ${country.population}</p>
-        <img src="${country.flags.png}" alt="Flag of ${country.name.official}">
-        <p>Languages: ${Object.values(languages)}</p>
-        `;
+    <h2>${country.name.official}</h2>
+    <p>Capital: ${country.capital}</p>
+    <p>Population: ${country.population}</p>
+    <img src="${country.flags.png}" 
+    alt="Flag of ${country.name.official}" width="200" height="100">
+    <p>Languages: ${languagesString}</p>
+  `;
+
   countryInfo.appendChild(div);
 }
+
+inputText.addEventListener('input', debounce(inputHandler, debounce_delay));
